d9a51f3dfd57c9e69c67d7a54a36589abb69f39a
Jan Roger Gerbig <jrgerbig@de.ibm.com>
2021-09-13 14:44:41 +0200

    Comments

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,9 @@
+/*
+This package converts measurements of the ruler tool from feet to meters and from miles to kilometers.
+5 ft -> 1.5 meter
+1  mile -> 1,61 kilometers
+In case there is an unknown unit nothing happens.
+ */
 Hooks.once('ready', () => {
     if(!game.modules.get('libruler')?.active && game.user.isGM){
         console.error("metric-ruler-labels | Module requires the 'libruler' module. Please install and activate it.");
@@ -8,12 +14,15 @@ Hooks.once('ready', () => {
             let label = wrapped();
             let regexFeet = /(\d*\.?\d*)\s?(?:ft\.?|feet)\.?\s?\[(\d*\.?\d*)\s?(?:ft\.?|feet)\.?\]/;
             let regexMiles = /(\d*\.?\d*)\s?(?:mi\.?|miles)\.?\s?\[(\d*\.?\d*)\s(?:mi\.?|miles)\.?\]/;
+            //Check if measurement is in feet
             let result = regexFeet.exec(label._text);
             if(result && result.length === 3) {
                 //Convert to meters and set label
                 label._text = label._text + " \n " + parseFloat(((result[1]/10)*3).toFixed(2)) + " m [" + parseFloat(((result[2]/10)*3).toFixed(2)) + " m]"
             }else{
+                //Check if measurement is in miles
                 result = regexMiles.exec(label._text);
+                //Convert to kilometers
                 if(result && result.length === 3){
                     label._text = label._text + " \n " + parseFloat((result[1]/0.62137).toFixed(2)) + " km [" + parseFloat((result[2]/0.62137).toFixed(2)) + " km]"
                 }
