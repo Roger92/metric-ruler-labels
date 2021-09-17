aa4cb54dd62fa96d6558b0de4cff0049281f4ec6
Jan Roger Gerbig <jrgerbig@de.ibm.com>
2021-09-17 23:53:07 +0200

    Added support for MeasureTemplates

--- scripts/module.js
+++ scripts/module.js
@@ -4,9 +4,11 @@ This package converts measurements of the ruler tool from feet to meters and fro
 1  mile -> 1,61 kilometers
 In case there is an unknown unit nothing happens.
  */
+
 Hooks.on("init", () => {
     registerSettings();
 });
+
 
 Hooks.once('ready', () => {
     if (!game.modules.get('lib-wrapper')?.active && game.user.isGM) {
@@ -22,60 +24,46 @@ Hooks.once('ready', () => {
             }
         }, { width: 600 }).render(true);
     } else {
+
+        //Handling of MeasureTemplate
+        libWrapper.register("metric-ruler-labels", "TemplateLayer.prototype._onDragLeftDrop", async function (wrapped, ...args) {
+            let wrappedResult = await wrapped(...args);
+            let measureTemplateSupport = game.settings.get("metric-ruler-labels", "measureTemplateSupport");
+
+            if(measureTemplateSupport && wrappedResult._object && wrappedResult._object.ruler){
+                wrappedResult._object.ruler.text = addMetricLabels(wrappedResult._object.ruler.text);
+            }
+            return wrappedResult;
+        }, 'MIXED');
+
+        //Handling of MeasureTemplate Preview
+        libWrapper.register("metric-ruler-labels", "MeasuredTemplate.prototype.refresh", async function (wrapped, ...args) {
+            let wrappedResult = await wrapped(...args);
+            let measureTemplateSupport = game.settings.get("metric-ruler-labels", "measureTemplateSupport");
+
+            if(measureTemplateSupport && wrappedResult && wrappedResult.ruler){
+                wrappedResult.ruler.text = addMetricLabels(wrappedResult.ruler.text);
+            }
+            return wrappedResult;
+        }, 'MIXED');
+
+        //Handling of Ruler and DragRuler
         libWrapper.register("metric-ruler-labels", "Ruler.prototype.measure", function (wrapped, ...args) {
             let wrappedResult = wrapped(...args);
-            let regexFeet = /^(\d*\.?\d*)\s?(?:ft\.?|feet)\s?(?:\[(\d*\.?\d*)\s?(?:ft\.?|feet)\])?$/;
-            let regexMiles = /^(\d*\.?\d*)\s?(?:mi\.?|miles)\s?(?:\[(\d*\.?\d*)\s(?:mi\.?|miles)\])?$/;
             let dragRulerSupportActive = game.settings.get("metric-ruler-labels", "dragRulerSupport");
 
             if (wrappedResult.label) {
                 let segment = wrappedResult;
                 //Loop over all prior segments of the ruler
                 do {
-                    //Check if measurement is in feet
-                    let regexResult = regexFeet.exec(segment.label._text);
-                    if (regexResult && regexResult.length === 3 && regexResult[1]) {
-                        //Convert to meters and set label
-                        segment.label._text = segment.label._text + " \n " + parseFloat(((regexResult[1] / 10) * 3).toFixed(2)) + " m";
-                        if (regexResult[2]) {
-                            segment.label._text = segment.label._text + " [" + parseFloat(((regexResult[2] / 10) * 3).toFixed(2)) + " m]";
-                        }
-                    } else {
-                        //Check if measurement is in miles
-                        regexResult = regexMiles.exec(segment.label._text);
-                        //Convert to kilometers and set label
-                        if (regexResult && regexResult.length === 3 && regexResult[1]) {
-                            segment.label._text = segment.label._text + " \n " + parseFloat((regexResult[1] / 0.62137).toFixed(2)) + " km";
-                            if (regexResult[2]) {
-                                segment.label._text = segment.label._text + " [" + parseFloat((regexResult[2] / 0.62137).toFixed(2)) + " km]";
-                            }
-                        }
-                    }
+                    segment.label.text = addMetricLabels(segment.label.text);
                     // Go to prior segment and convert label -> For the case that the ruler has waypoints
                     segment = segment.prior_segment;
                 } while (segment !== undefined && Object.keys(segment).length > 0);
 
             } else if (dragRulerSupportActive && Array.isArray(wrappedResult) && wrappedResult.length > 0) { //Handling for Dragruler Support
                 for (let i = 0; i < wrappedResult.length; i++) {
-                    //Check if measurement is in feet
-                    let regexResult = regexFeet.exec(wrappedResult[i].label.text);
-                    if (regexResult && regexResult.length === 3 && regexResult[1]) {
-                        //Convert to meters and set label
-                        wrappedResult[i].label.text = wrappedResult[i].label.text + " \n " + parseFloat(((regexResult[1] / 10) * 3).toFixed(2)) + " m";
-                        if (regexResult[2]) {
-                            wrappedResult[i].label.text = wrappedResult[i].label.text + " [" + parseFloat(((regexResult[2] / 10) * 3).toFixed(2)) + " m]";
-                        }
-                    } else {
-                        //Check if measurement is in miles and set label
-                        regexResult = regexMiles.exec(wrappedResult[i].label.text);
-                        //Convert to kilometers
-                        if (regexResult && regexResult.length === 3 && regexResult[1]) {
-                            wrappedResult[i].label.text = wrappedResult[i].label.text + " \n " + parseFloat((regexResult[1] / 0.62137).toFixed(2)) + " km";
-                            if (regexResult[2]) {
-                                wrappedResult[i].label.text = wrappedResult[i].label.text + " [" + parseFloat((regexResult[2] / 0.62137).toFixed(2)) + " km]";
-                            }
-                        }
-                    }
+                    wrappedResult[i].label.text = addMetricLabels(wrappedResult[i].label.text);
                 }
             }
             return wrappedResult;
@@ -85,6 +73,14 @@ Hooks.once('ready', () => {
 });
 
 function registerSettings() {
+    game.settings.register("metric-ruler-labels", "measureTemplateSupport", {
+        name: "metric-ruler-labels.settings.measureTemplateSupport.name",
+        hint: "metric-ruler-labels.settings.measureTemplateSupport.hint",
+        scope: "client",
+        config: true,
+        type: Boolean,
+        default: true,
+    });
     game.settings.register("metric-ruler-labels", "dragRulerSupport", {
         name: "metric-ruler-labels.settings.dragRulerSupport.name",
         hint: "metric-ruler-labels.settings.dragRulerSupport.hint",
@@ -93,4 +89,34 @@ function registerSettings() {
         type: Boolean,
         default: true,
     });
+}
+
+function addMetricLabels(text){
+    let regexFeet = /^(-?\d*\.?\d*)\s?(?:ft\.?|feet)\s?x?(\s?(-?\d*\.?\d*)\s?(?:ft\.?|feet))?(?:\[(-?\d*\.?\d*)\s?(?:ft\.?|feet)\])?$/;
+    let regexMiles = /^(-?\d*\.?\d*)\s?(?:mi\.?|miles)\s?x?(\s?(-?\d*\.?\d*)\s?(?:mi\.?|miles))?(?:\[(-?\d*\.?\d*)\s(?:mi\.?|miles)\])?$/;
+
+    let regexResult = regexFeet.exec(text);
+    if (regexResult && regexResult.length === 5 && regexResult[1]) {
+        //Convert to meters and set label
+        text = text + " \n " + parseFloat(((regexResult[1] / 10) * 3).toFixed(2)) + " m";
+        if(regexResult[3]) {
+            text = text + " x " + parseFloat(((regexResult[3] / 10) * 3).toFixed(2)) + " m";
+        }else if(regexResult[4]) {
+            text = text + " [" + parseFloat(((regexResult[4] / 10) * 3).toFixed(2)) + " m]";
+        }
+    } else {
+        //Check if measurement is in miles
+        regexResult = regexMiles.exec(text);
+        //Convert to kilometers and set label
+        if (regexResult && regexResult.length === 5 && regexResult[1]) {
+            text = text + " \n " + parseFloat((regexResult[1] / 0.62137).toFixed(2)) + " km";
+            if (regexResult[3]) {
+                text = text + " x " + parseFloat((regexResult[3] / 0.62137).toFixed(2)) + " km";
+            }else if(regexResult[4]){
+                text = text + " [" + parseFloat((regexResult[4] / 0.62137).toFixed(2)) + " km]";
+            }
+        }
+    }
+
+    return text;
 }
